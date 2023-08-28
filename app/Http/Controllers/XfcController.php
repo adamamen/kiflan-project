9475c4072b074c2f7e57ef5afaa07976ada9dc71
unknown <adamamen21@gmail.com>
2023-08-28 21:45:39 +0700

    update foto interior untuk XFC

--- app/Http/Controllers/XfcController.php
+++ app/Http/Controllers/XfcController.php
@@ -15,7 +15,7 @@ class XfcController extends Controller
         $mobil = Mobil::select('*')->where('kategori', '=', 'mobil_penumpang')->where('jenis', '=', 'XFC')->get()->toArray();
         $gallery = Gallery::select('*')->where('kategori', '=', 'mobil_penumpang')->where('jenis', '=', 'XFC')->get()->toArray();
         $contact = Kontak::select('*')->get()->toArray();
-
+        // dd(substr($gallery[8]['assets'], -14, 8));
         return view('mobil_penumpang.xfc.index', compact('title'), [
             'contact' => $contact,
             'mobil' => $mobil,
